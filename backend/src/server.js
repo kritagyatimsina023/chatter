f70641afa39189a0193c28dc09dd13a40cc83808
Kritagya Timsina <sahemtimsina@gmail.com>
2026-02-13 12:20:40 +0545

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,15 +3,29 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import messagesRouter from "./routes/messages.route.js";
+import path from "path";
 dotenv.config();
 
 const app = express();
+const __dirname = path.resolve();
+
 console.log(process.env.PORT);
 const port = process.env.PORT || 3000;
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messagesRouter);
 
+// make ready for deployment
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist"))); // taking dist from frontend and making it an static assets
+
+  // anything other then this /api/auth ot /api related works just send the react application meaning that both frontend and backend runs the same port
+  app.get("*", (_, res) => {
+    res.sendFile(
+      path.join(path.join(__dirname, "../frontend/dist/index.html")),
+    );
+  });
+}
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
