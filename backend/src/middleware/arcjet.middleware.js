80e193a23b939d48cc0735358d46dcb85d527019
Kritagya Timsina <sahemtimsina@gmail.com>
2026-02-15 11:18:35 +0545

    rate limiting bot fixed

--- backend/src/middleware/arcjet.middleware.js
+++ backend/src/middleware/arcjet.middleware.js
@@ -9,7 +9,7 @@ export const arcjetProtection = async (req, res, next) => {
         return res
           .status(429)
           .json({ message: "Rate limit exceeded , try again later" });
-      } else if (decision.reason.isBot) {
+      } else if (decision.reason.isBot()) {
         return res.status(429).json({ message: "Bot access denied" });
       } else {
         return res
