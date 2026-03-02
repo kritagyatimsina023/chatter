54780747654b4739cb85e5b553256cb121e3edf8
Kritagya Timsina <sahemtimsina@gmail.com>
2026-03-02 10:10:38 +0545

    send-message functionality implemented

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { useChatStore } from "../store/useChatStore";
 import { useAuthStore } from "../store/useAuthStore";
 import Chatheader from "./Chatheader";
@@ -10,6 +10,13 @@ const ChatContainer = () => {
   const { selectedUser, isMessagesLoading, getMessagesByUserId, messages } =
     useChatStore();
   const { authUser } = useAuthStore();
+  const messageEndRef = useRef(null);
+
+  useEffect(() => {
+    if (messageEndRef.current) {
+      messageEndRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [messages]);
 
   useEffect(() => {
     getMessagesByUserId(selectedUser._id);
@@ -38,11 +45,16 @@ const ChatContainer = () => {
                   )}
                   {message.text && <p className="mt-2">{message.text}</p>}
                   <p className="text-xs mt-1 opacity-75  flex items-center gap-1">
-                    {new Date(message.createdAt).toISOString().slice(11, 16)}
+                    {new Date(message.createdAt).toLocaleTimeString(undefined, {
+                      hour: "2-digit",
+                      minut: "2-digit",
+                    })}
                   </p>
                 </div>
               </div>
             ))}
+            {/* //scrolling target  */}
+            <div ref={messageEndRef} />
           </div>
         ) : isMessagesLoading ? (
           <MessagesLoadingSkeleton />
